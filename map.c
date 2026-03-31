e4aae87708e1bb282eea69dac7285f78c1dd5526
wind <juliangarzon867@gmail.com>
2026-03-30 20:39:52 -0500

    Fix: Resolución de colisiones y resizing de Hashmap

--- map.c
+++ map.c
@@ -5,19 +5,15 @@
 typedef struct node{
   char* key;
   void* value;
+  struct node* next;
 } Node;
 
 typedef struct hashmap{
+  int capacity;
   int size;
   Node** storage;
+  float loadFactor;
 } Hashmap;
-
-Hashmap* createHashmap(int capacity){
-  Hashmap* map = malloc(sizeof(Hashmap));
-  map->size = capacity;
-  map->storage = calloc(capacity, sizeof(Node*));
-  return map;
-}
 
 unsigned long hash(unsigned char *str) {
    unsigned long hash = 5381;
@@ -27,18 +23,44 @@ unsigned long hash(unsigned char *str) {
   return hash;
 }
 
+Hashmap* createHashmap(int capacity){
+  Hashmap* map = malloc(sizeof(Hashmap));
+  map->capacity = capacity;
+  map->storage = calloc(capacity, sizeof(Node*));
+  return map;
+}
+
+void resize(void* map){
+  Hashmap* m = (Hashmap *)map;
+  int new_capacity = m->capacity*2;
+  m->storage = calloc(new_capacity, sizeof(Node*));
+  m->capacity = new_capacity;
+  
+  for(int i = 0; i < m->capacity; i++){
+    Node* current = m->storage[i];
+    if(current!= NULL){
+      int index = (int)(hash((unsigned char*)current->key) % m->capacity);
+      m->storage[index] = current;
+    }
+  }
+}
+
 void insert(void* map, char* key, void* value ){
     Hashmap* m = (Hashmap *)map;
-    int index = (int)(hash((unsigned char*)key) % m->size);
+    int index = (int)(hash((unsigned char*)key) % m->capacity);
     Node* new_node = malloc(sizeof(Node));
     new_node->key = strdup(key);
     new_node->value = value;
+    new_node->next = m->storage[index];
     m->storage[index] = new_node;
+    if(((++m->size)/m->capacity) >= m->loadFactor){
+      resize(m);
+    }
 }
 
 void* get(void* map, char* key){
   Hashmap* m = (Hashmap *)map;
-  int index = (int)(hash((unsigned char*)key) % m->size);
+  int index = (int)(hash((unsigned char*)key) % m->capacity);
   return m->storage[index]->value;
 }
 
